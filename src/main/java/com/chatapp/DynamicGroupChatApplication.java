a3e733e7433e139c314a4e09b748109a3d885d82
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2023-12-13 21:16:07 +0530

    Stand-alone chat app

---
+++ src/main/java/com/chatapp/DynamicGroupChatApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicGroupChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicGroupChatApplication.class, args);
+	}
+
+}
